6467b8e526db46a20777b973d2ff73aa25a855e6
Andrew Darts <mandrewdarts@gmail.com>
2016-03-30 11:11:12 -0500

    add node server, npm start

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(port, function() {
+  console.log('Listening on '+port);
+})
